d268088139c4d21a28f741471f39c27adc21d0c6
xinyu <zwpq505899380@163.com>
2020-01-02 11:30:28 +0800

    添加注解@Primary、@PostConstruct说明

--- standard-project-starter/src/main/java/com/xinyu/simple/configuration/MappingConverterAdapter.java
+++ standard-project-starter/src/main/java/com/xinyu/simple/configuration/MappingConverterAdapter.java
@@ -1,7 +1,10 @@
 package com.xinyu.simple.configuration;
 
+import com.xinyu.simple.web.controller.form.QueryForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
@@ -76,4 +79,39 @@ public class MappingConverterAdapter {
             });
         }
     }
+
+
+
+    /*
+     *  @PostConstruct注解：
+     *      1、jDK5规范中增加的注解，修饰的方法会在服务器加载Servlet的时候运行，并且只会被服务器执行一次
+     *      2、用途：所在类的对象被创建，并加载完依赖注入后，开始执行被此注解修饰的方法
+     *      3、因spring是支持依赖注入，所以是spring自己来实现@PostConstruct的解析器功能
+     *      4、在spring项目，一个bean初始化过程中，方法执行先后顺序：Constructor > @Autowired > @PostConstruct,
+     *          即先执行完构造方法，再注入依赖，最后执行被@PostConstruct修饰的方法
+     *      5、对应注解@PreDestroy，则是bean销毁时执行
+     *      6、使用条件：只有非静态方法；不得有任何形参；返回值必须为void；不得抛出异常；方法只会被执行一次
+     **/
+    @PostConstruct
+    public void postConstruct() {
+        //执行对象或程序初始化工作，如：加载数据字典，修改程序启动参数等
+
+        //利用依赖注入后再执行的特性，修改springboot自动装配对象的属性，如上面addConversionConfig()方法、springboot各种组件配置的修改等
+
+    }
+
+    /**
+     *  @Primary注解：
+     *      1、问题：当一个接口有2个不同实现时,使用@Autowired注解时会报NoUniqueBeanDefinitionException异常信息。解决方案有两种-
+     *          ①使用@Qualifier注解，选择一个对象的名称指定某个唯一对象，比较常用
+     *          ②使用@Primary注解，指定某个类产生的对象进行优先注入
+     *      2、作用：当发生依赖注入时一个bean有多个候选者，则会挑选被标注为@Primary的bean
+     *      3、使用目标：方法或者类上
+     **/
+    @Bean
+    @Primary
+    public QueryForm createQueryForm(){
+        return new QueryForm();
+    }
+
 }
